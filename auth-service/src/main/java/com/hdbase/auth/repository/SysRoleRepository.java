5d7da723b5c496a71e0182b735fcb3a1af036fd5
huangdan1988 <261005152@qq.com>
2018-01-24 16:06:05 +0800

    建立基础版本

---
+++ auth-service/src/main/java/com/hdbase/auth/repository/SysRoleRepository.java
@@ -0,0 +1,11 @@
+package com.hdbase.auth.repository;
+
+
+import com.hdbase.auth.domain.SysRole;
+import com.hdbase.auth.repository.support.WiselyRepository;
+
+/**
+ * Created by wangyunfei on 2017/6/9.
+ */
+public interface SysRoleRepository extends WiselyRepository<SysRole,Long> {
+}
