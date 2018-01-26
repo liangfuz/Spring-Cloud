9ca0c7df8946b6defeda0f97e7427a446ffea5a0
liangfuz <838152053@qq.com>
2018-01-26 17:14:22 +0800

    修改Spring Cloud Config

--- product-service/src/main/java/com/hdbase/product/controller/ProductController.java
+++ product-service/src/main/java/com/hdbase/product/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.hdbase.product.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product")
 public class ProductController {
 
+	@Value("${members}")
+	private String members;
+
 	@RequestMapping("/get")
 	public String get(@RequestParam String name) {
-		return "hello "+name+"，this is product info";
+		return "hello "+name+"，this is product info ";
+	}
+
+	@RequestMapping("/listMembers")
+	public String listMembers() {
+		return "hello,our team members: "+members;
 	}
 
 }
